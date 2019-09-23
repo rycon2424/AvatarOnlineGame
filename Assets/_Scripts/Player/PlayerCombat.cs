265ac1e55a0c673810058cc564e7b0a2e1d94543
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2019-09-23 15:44:02 +0200

    Deleted Test Object/Code

--- Assets/_Scripts/Player/PlayerCombat.cs
+++ Assets/_Scripts/Player/PlayerCombat.cs
@@ -5,31 +5,5 @@ using Photon.Pun;
 
 public class PlayerCombat : UsingOnline
 {
-    [SerializeField]
-    private GameObject _cube;
 
-    void Start()
-    {
-        
-    }
-    
-    void Update()
-    {
-        if (pv.IsMine == false)
-        {
-            return;
-        }
-
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            pv.RPC("Spawn", RpcTarget.All);
-        }
-    }
-
-    [PunRPC]
-    void Spawn()
-    {
-        GameObject cube = Instantiate(_cube, transform.position, transform.rotation);
-        cube.transform.position += new Vector3(0, 3, 0);
-    }
 }
