75c753f11a1a9fbd7960ca184207b9655aba71e4
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2019-09-25 19:56:29 +0200

    Added script to destroy on time

---
+++ Assets/_Scripts/Utility/TimedDestroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedDestroy : MonoBehaviour
+{
+    public float time;
+
+    void Start()
+    {
+        Destroy(gameObject, time);
+    }
+
+}
