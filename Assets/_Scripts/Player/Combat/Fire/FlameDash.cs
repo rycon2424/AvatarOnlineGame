be253eebbfece89c8b3f5e779807481142759dae
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2019-09-26 18:54:46 +0200

    added flame dash attack animations fully working but no damage yet

---
+++ Assets/_Scripts/Player/Combat/Fire/FlameDash.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameDash : Moves
+{
+    public GameObject dashEffect;
+    public GameObject flameTrail;
+    private PlayerCombat _playerCombat;
+
+    public override void UseMove(PlayerCombat playerCombat)
+    {
+        base.UseMove(playerCombat);
+        _playerCombat = playerCombat;
+    }
+
+    public void EnableEffect()
+    {
+        dashEffect.SetActive(true);
+    }
+
+    public void DisableEffect()
+    {
+        Invoke("DisableDashEffect", 1.5f);
+    }
+
+    void DisableDashEffect()
+    {
+        dashEffect.SetActive(false);
+    }
+
+}
