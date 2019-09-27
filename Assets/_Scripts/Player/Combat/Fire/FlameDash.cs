3643a3314dc9b762eaf46604aaf52dc47289ca67
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2019-09-27 14:43:07 +0200

    kick of firebender does damage

--- Assets/_Scripts/Player/Combat/Fire/FlameDash.cs
+++ Assets/_Scripts/Player/Combat/Fire/FlameDash.cs
@@ -6,6 +6,8 @@ public class FlameDash : Moves
 {
     public GameObject dashEffect;
     public GameObject flameTrail;
+    public BoxCollider damageTrigger;
+    public int _damage;
     private PlayerCombat _playerCombat;
 
     public override void UseMove(PlayerCombat playerCombat)
@@ -17,16 +19,29 @@ public class FlameDash : Moves
     public void EnableEffect()
     {
         dashEffect.SetActive(true);
+        Debug.Log("enable trigger comp");
+        damageTrigger.enabled = true;
     }
 
     public void DisableEffect()
     {
         Invoke("DisableDashEffect", 1.5f);
+        damageTrigger.enabled = false;
     }
 
     void DisableDashEffect()
     {
+        Debug.Log("disable trigger comp");
         dashEffect.SetActive(false);
     }
 
+    void OnTriggerEnter(Collider collider)
+    {
+        PlayerController player = collider.gameObject.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            player.TakeDamage(_damage);
+            damageTrigger.enabled = false;
+        }
+    }
 }
