4bad0418855634bab86a3ff1b077f42420f60e89
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2019-09-27 19:04:18 +0200

    FireBender Shield Working

---
+++ Assets/_Scripts/Player/Combat/Fire/FlameShield.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameShield : Shield
+{
+    public int _damage;
+
+    void OnTriggerStay(Collider collider)
+    {
+        PlayerController player = collider.gameObject.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            player.TakeDamage(_damage);
+        }
+    }
+}
