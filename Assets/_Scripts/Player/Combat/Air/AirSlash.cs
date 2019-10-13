ec7be10504ff6b67e291e74412e72d2863bb4f4e
Flashcaptain <lex.zara@upcmail.nl>
2019-10-13 19:04:44 +0200

    Add airbender light and heavy

---
+++ Assets/_Scripts/Player/Combat/Air/AirSlash.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirSlash : ProjectileBased
+{
+    private PlayerCombat _playerCombat;
+
+    public override void UseMove(PlayerCombat playerCombat)
+    {
+        base.UseMove(playerCombat);
+        _playerCombat = playerCombat;
+    }
+
+    public void FireAirSlash()
+    {
+        Projectile projectile = Instantiate(_projectile, _spawnPosition[0].position, _spawnPosition[0].rotation);
+        projectile.transform.LookAt(_playerCombat.GetDirection());
+        projectile.Fired(_damage, _speed, _range);
+    }
+}
---
+++ Assets/_Scripts/Player/Combat/Air/Airleap.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airleap : RaycastBased
+{
+    [SerializeField]
+    protected float _downwardRayRange;
+
+    [SerializeField]
+    protected float _betweenRayRange;
+
+    [SerializeField]
+    protected Projectile _cloud;
+
+    [SerializeField]
+    protected float _cloudDuration;
+
+    [SerializeField]
+    protected LayerMask layerMask;
+
+    private GameObject _rayPoint;
+
+    public void AirBlast()
+    {
+        _rayPoint = Instantiate(_spawnPosition[0].gameObject, _spawnPosition[0].position, _spawnPosition[0].rotation);
+        RaycastHit main;
+        if (Physics.Raycast(_rayPoint.transform.position, _rayPoint.transform.forward, out main, _range, layerMask))
+        {
+            StartCoroutine(CastRays(main.distance));
+            return;
+        }
+        StartCoroutine(CastRays(_range));
+    }
+
+    private IEnumerator CastRays(float distance)
+    {
+        for (float i = 0; i < distance; i += _betweenRayRange)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(_rayPoint.transform.position + _rayPoint.transform.forward * i, -_rayPoint.transform.up, out hit, _downwardRayRange))
+            {
+                PlayerController player = hit.collider.GetComponent<PlayerController>();
+                if (player != null)
+                {
+                    player.TakeDamage(_damage);
+                }
+                else
+                {
+                    Projectile rock = Instantiate(_cloud, hit.point, Random.rotation);
+                    StartCoroutine(DestroyClouds(rock));
+                }
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        Destroy(_rayPoint.gameObject);
+    }
+
+    private IEnumerator DestroyClouds(Projectile projectile)
+    {
+        yield return new WaitForSeconds(_cloudDuration);
+        projectile.Destroy();
+    }
+}
