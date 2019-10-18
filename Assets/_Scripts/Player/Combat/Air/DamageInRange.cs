955df16ab338aca1e42ada1061b45b295d923b1f
Flashcaptain <lex.zara@upcmail.nl>
2019-10-18 19:36:39 +0200

    Add monk Ultimate

---
+++ Assets/_Scripts/Player/Combat/Air/DamageInRange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageInRange : Ranged
+{
+    private List<PlayerController> _players = new List<PlayerController>();
+    private PlayerCombat _playerCombat;
+
+    public override void UseMove(PlayerCombat playerCombat)
+    {
+        base.UseMove(playerCombat);
+        _playerCombat = playerCombat;
+    }
+
+    public void DamageAllInRange()
+    {
+        _players.AddRange(FindObjectsOfType<PlayerController>());
+        for (int i = 0; i < _players.Count; i++)
+        {
+            if (_playerCombat._playerController != _players[i] && Vector3.Distance(transform.position, _players[i].transform.position) < _range)
+            {
+                _players[i].TakeDamage(_damage);
+            }
+        }
+    }
+}
