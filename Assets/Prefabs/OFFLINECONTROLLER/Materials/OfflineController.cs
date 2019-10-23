836e112bc5e25d30aecfe21aaea5abc370850efe
rycon2424 <36225966+rycon2424@users.noreply.github.com>
2019-10-23 16:04:45 +0200

    Added OfflineTest

---
+++ Assets/Prefabs/OFFLINECONTROLLER/Materials/OfflineController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfflineController : MonoBehaviour
+{
+    public float animatorSpeed;
+    public Animator anim;
+    public GameObject playerCamera;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
+    void Update()
+    {
+        anim.speed = animatorSpeed;
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        anim.SetFloat("Horizontal", x);
+        anim.SetFloat("Vertical", z);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetBool("Running", true);
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+        }
+        RotateToLook();
+    }
+
+    void RotateToLook()
+    {
+        var CharacterRotation = playerCamera.transform.rotation;
+        CharacterRotation.x = 0;
+        CharacterRotation.z = 0;
+        transform.rotation = CharacterRotation;
+    }
+
+}
