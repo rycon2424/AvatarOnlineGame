f836a358710713e6e82942adeec9518ba1a70836
Flashcaptain <lex.zara@upcmail.nl>
2019-10-31 16:46:38 +0100

    Add combustion man ultimate

---
+++ Assets/_Scripts/Player/Combat/Fire/FireChestExplotion.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireChestExplotion : ProjectileBased
+{
+    [SerializeField]
+    private LineRenderer _lineRenderer;
+
+    [SerializeField]
+    private float _lineTime;
+
+    private PlayerCombat _playerCombat;
+    private Projectile _firedProjectile;
+    public override void UseMove(PlayerCombat playerCombat)
+    {
+        base.UseMove(playerCombat);
+        _playerCombat = playerCombat;
+    }
+
+    public void ShootChestExplotion()
+    {
+        RaycastHit hit;
+        _lineRenderer.SetPosition(0, _spawnPosition[0].position);
+        if (Physics.Raycast(_spawnPosition[0].position, _spawnPosition[0].forward, out hit, _range))
+        {
+            _lineRenderer.SetPosition(1, hit.point);
+            _firedProjectile = Instantiate(_projectile, hit.point, transform.rotation);
+        }
+        else
+        {
+            _lineRenderer.SetPosition(1, _spawnPosition[0].position + _spawnPosition[0].forward * _range);
+            _firedProjectile = Instantiate(_projectile, _spawnPosition[0].position + _spawnPosition[0].forward * _range, transform.rotation);
+        }
+        _firedProjectile.Fired(_damage, _speed, _speed, _playerCombat._playerController);
+        Invoke("RemoveLine", _lineTime);
+    }
+
+    private void RemoveLine()
+    {
+        _lineRenderer.SetPosition(0, Vector3.zero);
+        _lineRenderer.SetPosition(1, Vector3.zero);
+    }
+}
