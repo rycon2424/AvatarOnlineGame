f96a52c64d7e2fed5cc02e6ce7e75cf6585ebf07
Flashcaptain <36227316+Flashcaptain@users.noreply.github.com>
2020-01-29 20:51:17 +0100

    Add cheat (H) to fullyheal with cheatscript

---
+++ Assets/_Scripts/Player/Combat/CheatScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatScript : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerController _playerController;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            _playerController.TakeDamage(_playerController._health - _playerController._maxHealth, _playerController);
+        }
+    }
+}
